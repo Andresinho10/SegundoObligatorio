77b5807921ef307de4195d4e9b76ca57e0d46fbc
Andresinho10 <42248906+Andresinho10@users.noreply.github.com>
2018-08-09 15:20:24 -0300

    Add files via upload
    Limite de credito agregado

---
+++ SegundoObligatorio1.6/SegundoObligatorio1.4/SegundoObligatorio1.0/Tarjeta.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SegundoObligatorioAlpha
+{
+    class Tarjeta
+    {
+        #region Atributos
+       
+        Random rango = new Random();
+        public static int numero = Rango();
+        private bool personalizada;
+       
+        
+
+        #endregion
+
+        #region Propiedades
+
+        public int Numero
+        {
+        	get { return numero; }
+        }
+
+        public bool Personalizada
+        {
+            set { personalizada = value; }
+            get { return personalizada; }
+        }
+
+        #endregion
+
+        #region Constructores
+        //Constructor COMPLETO
+        public Tarjeta(bool cPersonalizada)
+        {
+           Personalizada = cPersonalizada;
+        }
+        //Constructor por defecto
+        public Tarjeta()
+        {
+           personalizada = true; 
+        }
+
+        //Metodo ToString()
+       
+        public override string ToString()
+        {
+            string frase = "\n\tSe ha creado correctamente la tarjeta Nro. " + "\n\t" +  Tarjeta.numero;
+            if (personalizada)
+            {
+            	frase += " Personalizada";
+            }
+            
+            else	
+            {
+            	frase += " Común";
+            }
+           
+            return frase;
+        }
+        
+        //Genera un nro. aleatorio para inicializar el atributo estatico "número"
+        public static int Rango()
+        {
+            Random rango = new Random();
+            int nroTarjeta = rango.Next(45001000,46001000);
+        	return nroTarjeta;
+        }
+       
+        #endregion
+
+        #region Operaciones
+        #endregion
+    }
+}
