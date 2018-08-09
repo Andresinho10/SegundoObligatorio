24fe3eecd98189be46d5c7743627e455c479083c
sebadlt <39435178+sebadlt@users.noreply.github.com>
2018-08-09 18:07:04 -0300

    Add files via upload
    YA quedó lo de la selección opcional de si es personalizada o no en el caso de credito

---
+++ Credito.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SegundoObligatorioAlpha
+{
+    class Credito : Tarjeta
+    {
+        #region Atributos
+
+        public string categoria;
+        private int limiteDeCredito ;
+        List<Pago> listaPagos;
+
+        #endregion
+
+        #region Propiedades
+        
+        public string Categoria
+        {
+            set { categoria = value; }
+            get { return categoria; }
+        }
+        
+
+        public int LimiteDeCredito
+        {
+            set
+            {
+                if (categoria == "Clasica")
+                {
+                    limiteDeCredito = 18000;
+                }
+                else if (categoria == "Plata")
+                {
+                    limiteDeCredito = 30000;
+                }
+                else if (categoria == "Oro")
+                {
+                    limiteDeCredito = 70000;
+                }
+                else if (categoria == "Platinium")
+                {
+                    limiteDeCredito = 120000;
+                }
+            }
+            get { return limiteDeCredito; }
+        }
+        #endregion
+
+        #region Constructores
+       //Constructor Completo
+       public Credito(bool cPersonalizada, string cCategoria)
+           : base( cPersonalizada)
+        {
+           Categoria = cCategoria;
+           LimiteDeCredito = limiteDeCredito;
+           Tarjeta.numero = numero + 1;
+
+        }
+
+        public Credito(bool cPersonalizada)
+            : base(cPersonalizada)
+        {
+            Categoria = categoria;
+            limiteDeCredito = 0;
+            Tarjeta.numero = numero + 1;
+        }
+        // Constructor por defecto
+        public Credito()
+            : base()
+        {
+            categoria = "Sin Datos";
+            limiteDeCredito = 0;
+            Tarjeta.numero=numero + 1;
+        }
+
+        //Metodo ToString()
+       
+        public override string ToString()
+        {
+            string frase = base.ToString();
+            frase += " de crédito \ncategoria " + categoria + " y su limite de crédito es de $"+ limiteDeCredito+"." ;
+            return frase;
+        }
+        
+        #endregion
+
+        #region Operaciones
+        
+        
+        
+        #endregion
+    }
+}
---
+++ Tarjeta.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SegundoObligatorioAlpha
+{
+    class Tarjeta
+    {
+        #region Atributos
+       
+        Random rango = new Random();
+        public static int numero = Rango();
+        private bool personalizada;
+       
+        
+
+        #endregion
+
+        #region Propiedades
+
+        public int Numero
+        {
+        	get { return numero; }
+        }
+
+        public bool Personalizada
+        {
+            set { personalizada = value; }
+            get { return personalizada; }
+        }
+
+        #endregion
+
+        #region Constructores
+        //Constructor COMPLETO
+        public Tarjeta(bool cPersonalizada)
+        {
+           Personalizada = cPersonalizada;
+        }
+        //Constructor por defecto
+        public Tarjeta()
+        {
+           personalizada = true; 
+        }
+
+        //Metodo ToString()
+       
+        public override string ToString()
+        {
+            string frase = "\nSe ha creado correctamente la tarjeta Nro. "  +  Tarjeta.numero;
+            if (personalizada)
+            {
+            	frase = " \nSe ha creado correctamente la tarjeta Personalizada ";
+            }
+           
+            return frase;
+        }
+        
+        //Genera un nro. aleatorio para inicializar el atributo estatico "número"
+        public static int Rango()
+        {
+            Random rango = new Random();
+            int nroTarjeta = rango.Next(45001000,46001000);
+        	return nroTarjeta;
+        }
+       
+        #endregion
+
+        #region Operaciones
+        #endregion
+    }
+}
