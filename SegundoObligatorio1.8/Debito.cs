0bd5bc0a29bd813f532fdad2ac2b74d6775d4831
sebadlt <39435178+sebadlt@users.noreply.github.com>
2018-08-09 21:31:41 -0300

    Add files via upload
    Algunas correcciones del formato en que se presentan los datos en consola.

---
+++ SegundoObligatorio1.8/Debito.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SegundoObligatorioAlpha
+{
+    class Debito : Tarjeta
+    {
+        #region Atributos
+
+        private int saldo;
+
+       #endregion
+
+        #region Propiedades
+
+        public int Saldo
+        {
+            set
+            {
+                if (value <= 0)
+                    throw new Exception("El saldo debe ser positivo");
+                else
+                    saldo = value;
+            }
+            get { return saldo; }
+        }
+        #endregion
+
+        #region Constructores
+        
+        //Constructor COMPLETO
+        public Debito(bool cPersonalizada)
+            : base(cPersonalizada)
+       {
+            saldo = 0;
+            Tarjeta.numero=numero + 1;
+       }
+
+        // Constructor por Defecto
+        public Debito()
+            : base ()
+        {
+            saldo = 0;
+            Tarjeta.numero=numero + 1;
+        }
+
+        //Metodo ToString();  -- Ver q frase va
+        public override string ToString()
+        {
+        	string frase = base.ToString();
+        	frase +=" de debito con saldo: $"+ saldo;
+            return frase;
+        }
+        
+        #endregion
+
+        #region Operaciones
+        #endregion
+    }
+}
