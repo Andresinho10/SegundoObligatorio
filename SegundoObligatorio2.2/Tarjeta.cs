6835298f8e5cb1b7d14342156678ef877d62a84d
sebadlt <39435178+sebadlt@users.noreply.github.com>
2018-08-12 14:50:16 -0300

    Add files via upload
    Se corrigieron algunas cosas que estaban mal.
    Ya la parte de "realizar pagos" y "cargar saldo" comienza a estar operativa.te permite buscar por la CI del cliente si este tiene tarjetas asociadas y te despliega una lista, dandote la opción para saber con que tarjeta queres operar.

---
+++ SegundoObligatorio2.2/Tarjeta.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SegundoObligatorioAlpha
+{
+    abstract class Tarjeta
+    {
+        #region Atributos
+       
+       
+        private static int Base = Rango();
+        private int numero;
+        private bool personalizada;
+     
+        
+
+        #endregion
+
+        #region Propiedades
+
+        public int Numero
+        {
+        	get { return numero;}
+        }
+
+        public bool Personalizada
+        {
+            set { personalizada = value; }
+            get { return personalizada; }
+        }
+
+        #endregion
+
+        #region Metodos
+        
+        //Constructor COMPLETO
+        public Tarjeta(bool cPersonalizada)
+        {
+           Personalizada = cPersonalizada;
+           if (Personalizada)
+           {
+               numero = 0;
+               Base = Base - 1;
+           }
+           else
+               numero = Base + 1;
+               Base++;
+           
+        }
+
+        //Metodo ToString()
+       
+        public override string ToString()
+        {
+            string frase = "\n\tTarjeta Nro."  + numero;
+            if (personalizada)
+            {
+            	frase = " \n\tTarjeta PERSONALIZADA (S/N)";
+            }
+           
+            return frase;
+        }
+        
+        //Genera un nro. aleatorio para inicializar el atributo estatico "número"
+        public static int Rango()
+        {
+            Random rango = new Random();
+            int nroTarjeta = rango.Next(45001000,46001000);
+        	return nroTarjeta;
+        }
+       
+        #endregion
+
+     
+    }
+}
