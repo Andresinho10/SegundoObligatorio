cb1ec666439538ffe2e4f1e1252546c7e2df22fd
sebadlt <39435178+sebadlt@users.noreply.github.com>
2018-08-13 17:44:16 -0300

    Add files via upload

---
+++ SegundoObligatorio2.3/Credito.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SegundoObligatorioAlpha
+{
+    class Credito : Tarjeta
+    {
+        #region Atributos
+
+        public string categoria;
+        private int limiteDeCredito ;
+        public static List<Pago> listaPagos;
+
+        #endregion
+
+        #region Propiedades
+        
+        public string Categoria
+        {
+            set { categoria = value; }
+            get { return categoria; }
+        }
+        
+
+        public int LimiteDeCredito
+        {
+        	set {limiteDeCredito=value;}
+         
+            get { return limiteDeCredito; }
+        }
+        
+        
+        #endregion
+
+        #region Metodos
+        
+       //Constructor COMPLETO
+       public Credito(bool cPersonalizada, string cCategoria,int cLimite)
+           : base( cPersonalizada)
+         {
+       	   Categoria = cCategoria;
+           LimiteDeCredito = cLimite;
+           listaPagos=new List<Pago>();
+         }
+
+
+        //Metodo ToString()
+       
+        public override string ToString()
+        {
+            string frase = base.ToString();
+            frase += " de CRÉDITO - CATEGORÍA " + categoria + " - LÍMITE DE CRÉDITO: $"+ limiteDeCredito+"." ;
+            return frase;
+        }
+        
+        
+        public void AgregarPago(Pago pago)
+        {
+        	listaPagos.Add(pago);
+        }
+        
+       
+         
+        #endregion
+
+    
+    }
+}
