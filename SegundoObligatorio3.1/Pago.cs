5aad36a8f4c11119fef71500a79fe0718c39d3fa
Andresinho10 <42248906+Andresinho10@users.noreply.github.com>
2018-08-17 15:15:22 -0300

    Add files via upload

---
+++ SegundoObligatorio3.1/Pago.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SegundoObligatorioAlpha
+{
+    class Pago
+    {
+        #region Atributos
+
+        private DateTime fecha;
+        private int monto;
+      
+
+        #endregion
+
+        #region Propiedades
+        public DateTime Fecha
+        {
+            set
+            {
+                if (DateTime.Compare(value, DateTime.Today) > 0)
+                    throw new Exception("La fecha de pago no puede ser futura.");
+                else
+                    fecha = value;
+            }
+            get { return fecha; }
+        }
+
+        public int Monto
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    monto = value;
+                }
+                else
+                {
+                    throw new Exception("El monto del pago debe ser positivo.");
+                }
+            }
+            get { return monto; }
+        }
+            
+       
+        #endregion
+
+        #region Constructores
+        //Constructor Completo
+        public Pago(DateTime cFecha, int cMonto)
+        {
+            Fecha = cFecha;
+            Monto = cMonto;
+            
+        }
+
+        //Metodo ToString(); 
+       
+        public override string ToString()
+        {
+        	string frase = "Pago del día " + fecha.ToShortDateString() + " por $" + monto;
+           
+            return frase;
+        }
+        
+        #endregion
+
+      
+    }
+}
