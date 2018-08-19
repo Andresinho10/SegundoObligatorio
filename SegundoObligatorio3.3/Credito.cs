e3a3a4b93e41c5b3d70f87e1d167059ee0233782
sebadlt <39435178+sebadlt@users.noreply.github.com>
2018-08-19 11:47:54 -0300

    Add files via upload
    -Se cambió el método ToString() de la clase cliente
    -Se eliminó el atributo "cantidadDeTarjetas"
    -Se cambió el Namespace a "SegundoObligatorio"

---
+++ SegundoObligatorio3.3/Credito.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SegundoObligatorio
+{
+    class Credito : Tarjeta
+    {
+        #region Atributos
+
+        public string categoria;
+        private int limiteDeCredito ;
+        public List<Pago> listaPagos;
+
+        #endregion
+
+        #region Propiedades
+        
+        public string Categoria
+        {
+            set { categoria = value; }
+            get { return categoria; }
+        }
+        
+
+        public int LimiteDeCredito
+        {
+        	set {limiteDeCredito=value;}
+         
+            get { return limiteDeCredito; }
+        }
+        
+        
+        #endregion
+
+        #region Constructores
+        
+       //Constructor COMPLETO
+       public Credito(bool cPersonalizada, string cCategoria,int cLimite)
+           : base( cPersonalizada)
+         {
+       	   Categoria = cCategoria;
+           LimiteDeCredito = cLimite;
+           listaPagos=new List<Pago>();
+         }
+
+        #endregion
+
+        #region Operaciones
+
+        //Metodo ToString()
+        public override string ToString()
+        {
+            string frase = base.ToString();
+            frase += " de CRÉDITO - CATEGORÍA " + categoria + " - LÍMITE DE CRÉDITO: $"+ limiteDeCredito+"." ;
+            return frase;
+        }
+      
+        //Verifica si un pago no supera el límite de credito de una tarjeta.
+        public void VerificarLimite(Pago pago, Credito tarjeta)
+        {
+        	listaPagos.Add(pago);
+            int suma=0;
+        	foreach(Pago pay in listaPagos)
+        	{
+        		suma +=pay.Monto;
+        		if (suma>tarjeta.LimiteDeCredito) 
+        		{
+        			listaPagos.Remove(pago);
+        			throw new Exception("\n\tLímite de crédito superado.No se ha podido ingresar el pago.");
+        		}
+        		
+            }
+        	
+        }
+               
+        #endregion
+  
+    }
+}
